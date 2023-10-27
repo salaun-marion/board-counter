ac3770040c85558a210051946452e6dd1e6f8f20
Marion SALAUN <marion.salauen.001@student.uni.lu>
2023-10-27 11:15:06 +0200

    First commit to initiate.

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Hearder(){
+    return(
+        <div className="title">
+            <h1>Community-driven</h1>
+            <h1 className = "color-title">Roadmap</h1>
+        </div>
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+const Wrap = ({ children }) => <div className="wrap">{children}</div>
+
+export default function Main () {
+    return(
+        <Wrap>
+            <div className="bp-container">
+                    {/* <img className="progressionbar"></img> */}
+                    <h5>Blabla</h5>
+            </div>
+        </Wrap>
+    )
+} 
+    
+
