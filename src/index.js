1e43cca9b151ea06da78ce3aa84e1e56139336b6
Marion SALAUN <marion.salauen.001@student.uni.lu>
2023-10-28 12:29:51 +0200

    Styling register panel done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from 'react-dom/client';
 import "./styles/font.css";
 import "./styles/style.css";
 
@@ -9,22 +9,6 @@ import Register from "./components/Register";
 
 const container = document.getElementById("root")
 const root = createRoot(container); 
-
-
-// class App extends React.Component {
-//     state = { fadingOut: false }
-//     fadeOut = () => this.setState({fadingOut: true});
-  
-//     render() {
-//       return (
-//         <div style={this.state.fadingOut?{filter:'blur(2px)'}:false }>
-//             <Header/>
-//             <Main/>
-//         </div>
-//       )
-//     }
-// }
-
 
 root.render(
     <div>
