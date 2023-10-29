d8a2a12f7f4fffbb25f6feaf7fdd209e8c613842
Marion SALAUN <marion.salauen.001@student.uni.lu>
2023-10-29 22:31:29 +0100

    3 columns created. CSS adjusted.

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,8 @@ export default function Main () {
 
     const[modal, setModal] = useState(false)
     const[count, setCount] = useState(0)
-    const[tinypanels, setTinyPanel] = useState([
+
+    const tinypanels=[
         {
             title:"Custom Fields",
             description:"Add custom fields to your passwords.",
@@ -20,8 +21,105 @@ export default function Main () {
             description:"Store other secrets than a password such as notes.",
             pro_off_on:"off",
             counter:0
+        },
+        {
+            title:"Custom ACL",
+            description:"Store other secrets than a password such as notes. ",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"Keepass as an offline client",
+            description:"Bi-directional sync with keepass file format to work offline.",
+            pro_off_on:"off",
+            counter:0
+        },
+        {
+            title:"Google SSO",
+            description:"Authenticate with Google.",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"KeyCloak SSO",
+            description:"Authenticate with KeyCloak.",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"RBAC",
+            description:"Role Based Access Control for both user interface actions and API endpoints.",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"TOTP Authenticator",
+            description:"Use passbolt to store one time passwords and easily share them securely with team members.",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"Advanced Audit logs",
+            description:"More options to see who is doing what on passbolt.",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"Microsoft SSO",
+            description:"Authenticate with Microsoft.",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"Account Recovery",
+            description:"Users who lost their passphrase or private key can recover their account.",
+            pro_off_on:"off",
+            counter:0
+        },
+        {
+            title:"Forms auto-save",
+            description:"Passbolt remembers passwords from web pages.",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"Mobile app",
+            description:"Passwords are available on Android and iPhones.",
+            pro_off_on:"on",
+            counter:0
+        },
+        {
+            title:"Internationalization",
+            description:"The interface and documentation are translatable in mutliple languages.",
+            pro_off_on:"on",
+            counter:0
         }
-    ]);
+    ]
+
+    const backpanels=[
+        {
+            title : "In Backlog",
+            color : "red",
+            card : tinypanels,
+            start:0,
+            end:4
+        },
+        {
+            title : "In Progress",
+            color : "yellow",
+            card : tinypanels,
+            start:4,
+            end:9
+        },
+        {
+            title : "Completed",
+            color : "green",
+            card : tinypanels,
+            start:9,
+            end:15
+        },
+    ]
+
 
     const toggleModal = () => {
         setModal(!modal) 
@@ -47,24 +145,28 @@ export default function Main () {
    
     return (
         <div className="wrap">
-            <BackPanel
-                title = "In Backlog"
-                color="red"
-                card = {
-                    tinypanels.map((tinypanel)=> {
-                        return(
-                            <Tinypanel 
-                                key={uuidv4()}
-                                title={tinypanel.title}
-                                description={tinypanel.description}
-                                pro_off_on={tinypanel.pro_off_on}
-                                count={tinypanel.counter}
-                                submitted={checkSubmission}
-                            />
-                        )
-                    })
-                }
-            />
+            {backpanels.map((backpanel)=> {
+                return(
+                    <BackPanel
+                    title={backpanel.title} 
+                    color={backpanel.color}
+                    card={
+                        tinypanels.slice(backpanel.start,backpanel.end).map((tinypanel)=> {
+                            return(
+                                <Tinypanel 
+                                    key={uuidv4()}
+                                    title={tinypanel.title}
+                                    description={tinypanel.description}
+                                    pro_off_on={tinypanel.pro_off_on}
+                                    count={tinypanel.counter}
+                                    submitted={checkSubmission}
+                                />
+                            )
+                        })
+                    }
+                    />
+                )
+            })}
             {modal && (
                 <div className="modal">
                     <div className= "overlay" onClick={toggleModal}>
